6b6a607cb724431e4ce26c29bf6a024ee9676a20
Hazeland-hx <82297981+Hazeland-hx@users.noreply.github.com>
2021-07-20 00:27:36 +0800

    Update firebase-connector.js

--- scripts/firebase-connector.js
+++ scripts/firebase-connector.js
@@ -39,3 +39,30 @@ let dblocation = firebase.database().ref('deck/flashcards');
 // Anything that was in this location will be overwritten
 // Thus, a write operation also does an update
 dblocation.set(myFlashcards);
+
+// Make the database point to the location root -> deck -> flashcards
+// If the location doesn't exist is will be created
+firebase.database().ref('deck/flashcards'); 
+ 
+// Remove myFlashcards from the database
+firebase.database.remove()
+
+// As before,make the database point to the location where the data exists
+// If the location doesn't exist it will be created but there will be nothing to retirieve
+let fc = firebase.database().ref('deck/flashy');
+
+// A variable to store the JSON results in a human readable format
+let jsonString;
+// Tell Firebase to retrieve your data
+
+fc.on("value", function(retrieve) {
+    //Get the raw JSON data back from the database
+    let queryData = retrieve.val();
+    // Turn the data into a JSON String
+    jsonString = JSON.stringify(queryData);
+});
+
+// Print the data out as a JSON string or otherwise manipulate it
+console.log (' JSON string:' + jsonString )
+
+
