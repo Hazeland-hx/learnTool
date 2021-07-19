844904fca102c2c1e6a8b5c5422d18a5b8265a5e
Hazeland-hx <82297981+Hazeland-hx@users.noreply.github.com>
2021-07-19 23:14:37 +0800

    Update and rename index.js to firebase-connector.js

---
+++ scripts/firebase-connector.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.7.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.7.1/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDO28bKdG5tpw_BrM0own5jzcZnie1TnWA",
+    authDomain: "best-learning-tool-21387.firebaseapp.com",
+    projectId: "best-learning-tool-21387",
+    storageBucket: "best-learning-tool-21387.appspot.com",
+    messagingSenderId: "282849610781",
+    appId: "1:282849610781:web:598cee1b1dbbfb54870af4",
+    measurementId: "G-447EF9KXRC"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
--- scripts/index.js
+++
@@ -1,7 +0,0 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
-
-function multiply(num1,num2) {
-  let result = num1 * num2;
-  return result;
-}
