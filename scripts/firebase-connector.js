6e378357ec02388398e60171c808ff47e8006edd
Hazeland-hx <82297981+Hazeland-hx@users.noreply.github.com>
2021-07-19 23:57:59 +0800

    Update firebase-connector.js

--- scripts/firebase-connector.js
+++ scripts/firebase-connector.js
@@ -1,5 +1,5 @@
 // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
     apiKey: "AIzaSyDO28bKdG5tpw_BrM0own5jzcZnie1TnWA",
     authDomain: "best-learning-tool-21387.firebaseapp.com",
@@ -7,8 +7,8 @@
     projectId: "best-learning-tool-21387",
     storageBucket: "best-learning-tool-21387.appspot.com",
     messagingSenderId: "282849610781",
-    appId: "1:282849610781:web:ea557c63a6021652870af4",
-    measurementId: "G-FLE3SHLRND"
+    appId: "1:282849610781:web:955b1d317a40720d870af4",
+    measurementId: "G-XF4J1GYWG9"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
